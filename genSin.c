02e33945b0fc758fc76e0622f534c51512a33933
Dakota Williams <dwms9395@verizon.net>
2015-06-14 18:24:44 -0400

    stub for generating a sound

---
+++ genSin.c
@@ -0,0 +1,7 @@
+
+#include "genSin.h"
+
+void genSin(int* data, int* dataSize, int bitRate, float phase, float freq, float length) {
+	
+}
+
---
+++ genSin.h
@@ -0,0 +1,8 @@
+
+#ifndef __GENSIN_H__
+#define __GENSIN_H__
+
+void genSin(int* data, int* dataSize, int bitRate, float phase, float freq, float length);
+
+#endif
+
